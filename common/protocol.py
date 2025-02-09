76999a9a665350363b11101d6a3c1e24cbf4af47
Ruichen Zhang <374992826@qq.com>
2025-02-09 10:56:25 -0500

    add support for Chatmsg

--- common/protocol.py
+++ common/protocol.py
@@ -1,11 +1,16 @@
 import struct
+import uuid
+import time
+from message import Chatmsg
 
 class Protocol:
     # constant for message type
     # request
     REQ_LOGIN_1 = 1
     REQ_LOGIN_2 = 2
-
+    REQ_SEND_MSG = 3
+    REQ_READ_MSG = 4
+    REQ_LIST_MESSAGES = 5
 
     # response
     RESP_USER_EXISTING = 101
@@ -13,87 +18,91 @@ class Protocol:
     RESP_LOGIN_SUCCESS = 103
     RESP_LOGIN_FAILED = 104
 
-
-
     @staticmethod
-    def encode_obj(self, obj):
+    def encode_obj(obj):
         """
-        serialize py objs
+        Serialize Python objects to binary format.
         """
-        
         if isinstance(obj, int):
-            # define int as type 0x00 + 8-byte signed integer
             return b'\x00' + struct.pack('!q', obj)
 
+        elif isinstance(obj, float):
+            return b'\x01' + struct.pack('!d', obj)  # 8-byte float
+
         elif isinstance(obj, str):
-            # define str as type 0x01 + 4-byte length + UTF-8 byte array
             data = obj.encode('utf-8')
             length = len(data)
-            return b'\x01' + struct.pack('!I', length) + data
+            return b'\x02' + struct.pack('!I', length) + data
 
         elif isinstance(obj, list):
-            # define list as type 0x02 + 4-byte size + encode_obj(elements)
-            encoded_items = []
-            for item in obj:
-                encoded_items.append(self.encode_obj(item))
-            total = b''.join(encoded_items)
-            return b'\x02' + struct.pack('!I', len(obj)) + total
+            encoded_items = [Protocol.encode_obj(item) for item in obj]
+            return b'\x03' + struct.pack('!I', len(obj)) + b''.join(encoded_items)
 
         elif isinstance(obj, dict):
-            # define dict as type 0x03 + 4-byte size + encode_obj(key) + encode_obj(value)
-            # we define key should be str
             encoded_items = []
             for k, v in obj.items():
-                encoded_key = self.encode_obj(str(k))
-                encoded_value = self.encode_obj(v)
+                encoded_key = Protocol.encode_obj(str(k))
+                encoded_value = Protocol.encode_obj(v)
                 encoded_items.append(encoded_key + encoded_value)
-            total = b''.join(encoded_items)
-            return b'\x03' + struct.pack('!I', len(obj)) + total
+            return b'\x04' + struct.pack('!I', len(obj)) + b''.join(encoded_items)
+
+        elif isinstance(obj, Chatmsg):
+            # Define Chatmsg as type 0x04, then encode it as a dictionary
+            encoded_chatmsg = Protocol.encode_obj(obj.to_dict())
+            return b'\x05' + encoded_chatmsg
 
         else:
             raise TypeError(f"Unsupported type: {type(obj)}")
-        
+
     @staticmethod
-    def decode_obj(self, data, offset=0):
+    def decode_obj(data, offset=0):
+        """
+        Deserialize binary data back to Python objects.
+        """
         type_code = data[offset]
         offset += 1
 
         if type_code == 0x00:
-            # int64
             (val,) = struct.unpack_from('!q', data, offset)
             offset += 8
             return val, offset
 
         elif type_code == 0x01:
-            # string
+            (val,) = struct.unpack_from('!d', data, offset) 
+            offset += 8
+            return val, offset
+
+        elif type_code == 0x02:
             (str_len,) = struct.unpack_from('!I', data, offset)
             offset += 4
-            s = data[offset:offset+str_len].decode('utf-8')
+            s = data[offset:offset + str_len].decode('utf-8')
             offset += str_len
             return s, offset
 
-        elif type_code == 0x02:
-            # list
+        elif type_code == 0x03:
             (list_size,) = struct.unpack_from('!I', data, offset)
             offset += 4
             arr = []
             for _ in range(list_size):
-                item, offset = self.decode_obj(data, offset)
+                item, offset = Protocol.decode_obj(data, offset)
                 arr.append(item)
             return arr, offset
 
-        elif type_code == 0x03:
-            # dict
+        elif type_code == 0x04:
             (dict_size,) = struct.unpack_from('!I', data, offset)
             offset += 4
             d = {}
             for _ in range(dict_size):
-                # key
-                key, offset = self.decode_obj(data, offset)
-                # value
-                value, offset = self.decode_obj(data, offset)
+                key, offset = Protocol.decode_obj(data, offset)
+                value, offset = Protocol.decode_obj(data, offset)
                 d[key] = value
             return d, offset
 
+        elif type_code == 0x05:
+            # Decode dictionary first, then convert to Chatmsg
+            chatmsg_dict, offset = Protocol.decode_obj(data, offset)
+            return Chatmsg.from_dict(chatmsg_dict), offset
+
         else:
             raise ValueError(f"Unknown type code: {type_code}")
+
