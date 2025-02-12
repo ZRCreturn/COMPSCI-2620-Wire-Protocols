bd8cac80587a113cbdca3444825992b26675ff14
Ruichen Zhang <374992826@qq.com>
2025-02-11 23:03:49 -0500

    add support for json

--- common/utils.py
+++ common/utils.py
@@ -1,6 +1,8 @@
+import json
 import struct
 import bcrypt
 from common.protocol import Protocol
+from common.message import Chatmsg
 
 def recv_data(sock):
     # receive 12 bytes header
@@ -41,3 +43,51 @@ def hash_pwd(password):
 
 def check_pwd(password, hashed_password):
     return bcrypt.checkpw(password.encode(), hashed_password.encode())    
+
+class CustomJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Chatmsg):
+            return obj.__dict__
+        return super().default(obj)
+
+def send_data_json(sock, msg_type, data):
+    if data is None:
+        payload = b""
+    else:
+        payload = json.dumps(data, cls=CustomJSONEncoder).encode('utf-8')
+
+    data_len = len(payload)
+    header = struct.pack('!QI', msg_type, data_len)
+    sock.sendall(header + payload)
+
+def decode_json(obj):
+    if isinstance(obj, dict) and "sender" in obj and "message" in obj:
+        return Chatmsg(**obj)  
+    elif isinstance(obj, list):
+        return [decode_json(item) for item in obj] 
+    return obj  
+
+def recv_data_json(sock):
+    header = sock.recv(12)
+    if len(header) < 12:
+        return None, None
+
+    msg_type, data_len = struct.unpack("!QI", header)
+
+    if data_len == 0:
+        return msg_type, None
+    
+    payload = b""
+    while len(payload) < data_len:
+        chunk = sock.recv(data_len - len(payload))
+        if not chunk:
+            return None, None 
+        payload += chunk
+
+    try:
+        obj = json.loads(payload.decode('utf-8'))
+        obj = decode_json(obj)  
+    except json.JSONDecodeError:
+        return None, None  
+
+    return msg_type, obj
